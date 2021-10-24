490ba7408ff648325176a30a566ad8ae12c55483
aashish18verma <aashish18verma@gmail.com>
2021-10-24 20:50:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthenticationService } from './service/authentication.service';
+import { UserService } from './service/user.service';
+import { AuthInterceptor } from './intreceptor/auth.interceptor';
+import { notificationModule } from './notification.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    notificationModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthenticationService, UserService,
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/enum/header-type.enum.ts
@@ -0,0 +1,5 @@
+export enum
+headerType{
+    AUTHORIZATION = 'AUTHORIZATION',
+    JWT_TOKEN = 'Jwt-Token',
+}
